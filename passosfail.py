ab3ecd0556150c6e60f7c28e861f7d98c7a5c701
lujalaaa <lujalamdhr345@gmail.com>
2025-08-23 21:56:15 +0545

    Update passosfail.py

--- passosfail.py
+++ passosfail.py
@@ -8,7 +8,7 @@ Created on Sun Aug 10 14:04:12 2025
  if it requires total 40% and at least 33% in each subject 
 to pass Assume three subject and 
 take marks as an input from user '''
-# let us assume there are 4 subjects 
+# let us assume there are 3 subjects 
 Math=eval(input('enter marks of maths: '))
 Science=eval(input('enter marks of science: '))
 English=eval(input('enter marks of English: '))

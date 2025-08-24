dd9201b7c9158af5ed899d7c2560c28b65bc1233
lujalaaa <lujalamdhr345@gmail.com>
2025-08-24 20:26:27 +0545

    multiplication table using loops

---
+++ multiplication.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 24 20:01:01 2025
+
+@author: lujal
+"""
+
+"""multiplication table using for loop and also using while loop"""
+print("Using For loop:")
+for x in range(1,10):
+  print('5*',x,"=",5*x) 
+  
+  
+  
+  
+print("using while loop")
+i=1
+while i<10:
+    print("6*",i,"=",6*i)
+    i+=1
+    

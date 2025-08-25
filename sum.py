8066d998953f40486f85433468f5777cc36cab9a
lujalaaa <lujalamdhr345@gmail.com>
2025-08-25 20:40:18 +0545

    sum of 1st n natural numbers

---
+++ sum.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 25 18:44:56 2025
+
+@author: lujal
+"""
+
+"""WAP to find the sum of 1st nth natural number"""
+n = int(input("Enter the value of n: "))
+sum_natural = n * (n + 1) // 2
+print(f"The sum of the first {n} natural numbers is {sum_natural}")

6e2f0a30935b60fd436bac351b2380098873eaa7
lujalaaa <lujalamdhr345@gmail.com>
2025-08-30 21:06:28 +0545

    wipe content

---
+++ wipe.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 30 21:02:34 2025
+
+@author: lujala
+"""
+
+# File path
+file_path = "G:\\lujalaAI\\demo4.txt"
+
+# Open in write mode
+with open(file_path, "w") as f:
+    pass  # nothing is written, so file becomes empty
+
+print("File contents wiped out successfully!")
